652e4209d818b056dedeae3628ef34c2dea9d9d8
mkm1879 <mkm1879@gmail.com>
2014-02-27 15:53:43 -0500

    Cleaned up extra files

--- PublishJitterDot.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-e:
-cd \EclipseJava\JitterDot\bin
-
-"C:\Program Files\Java\jdk1.7.0_25\bin\jar.exe" -cfm ..\JitterDot.jar ..\JitterDot.MF edu\clemson\lph\jitter edu\clemson\lph\security edu\clemson\lph\controls\GPSTextField.class edu\clemson\lph\dialogs 
-cd ..
--- src - Copy/edu/clemson/lph/jitter/files/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package edu.clemson.lph.jitter.files;
-
-@SuppressWarnings("serial")
-public class InvalidInputException extends Exception {
-	public InvalidInputException( String sError ) {
-		super( sError );
-	}
-}
